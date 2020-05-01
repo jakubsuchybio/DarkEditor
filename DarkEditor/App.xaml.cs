6db57c103015bf25bc76f1a1abf5557d13075ce0
Kuba Suchy <suchyj@btlnet.com>
2020-05-01 18:06:32 +0200

    Add project files.

---
+++ DarkEditor/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace DarkEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+
+            if (e.Args.Length == 1)
+                wnd.Initialize(e.Args[0]);
+
+            wnd.Show();
+        }
+    }
+}
---
+++ DarkEditor/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.IO;
+using System.Windows.Documents;
+
+namespace DarkEditor
+{
+    public partial class MainWindow : MetroWindow
+    {
+        private FileStream _fs;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Initialize(string filePath)
+        {
+            Title = $"DarkEditor - File: {filePath}";
+
+            _fs = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            using (var reader = new StreamReader(_fs, leaveOpen: true))
+                textBox.Text = reader.ReadToEnd();
+
+            textBox.CaretIndex = 0;
+            textBox.Focus();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _fs.SetLength(0);
+            using (var writer = new StreamWriter(_fs))
+                writer.Write(textBox.Text);
+            base.OnClosed(e);
+        }
+    }
+}
